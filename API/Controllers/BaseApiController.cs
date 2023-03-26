3ed8c02df481bb8972e055ac7859d14cab28d5e3
a moazeni <55484136+magnumAv@users.noreply.github.com>
2023-03-27 01:38:50 +0430

    End section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : Controller
+    {
+      
+      
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -7,9 +7,15 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API
 {
@@ -28,11 +34,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-            {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
+
+            services.AddApplicationServices(_config);
             services.AddControllersWithViews();
+            services.AddCors();
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,9 +58,11 @@ namespace API
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCors(s => s.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //
+            app.UseAuthentication();
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
